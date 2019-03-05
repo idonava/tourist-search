eb0c79ec4041b615b25dfed2636fb065a93a9f1d
Idonava <idonava@gmail.com>
2019-03-06 01:15:46 +0200

    fix cors 2

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ const handle = app.getRequestHandler()
 app.prepare()
 .then(() => {
   const server = express()
-
+  server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   server.use('/routing', routing);
   server.use('/db', db);
-  server.options('*', cors())
 
 
 
