1cd0603aec6ce2fa82645c1839d1602df11ee34b
Idonava <idonava@gmail.com>
2019-03-06 01:31:27 +0200

    now fix

--- libs/request.js
+++ libs/request.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_HOST = "https://carlos-rails-api.herokuapp.com";
+const API_HOST = "https://tourist-search.now.sh";
 
 const getUrl = endpoint => API_HOST + endpoint;
 
 export const post = async (endpoint, data) => {
-  console.log('hello from post ',`https://tourist-search.now.sh${endpoint}`)
+  console.log('hello from post ',`http://localhost:3100${endpoint}`)
   // const url = getUrl(endpoint);
-  return axios.post(`https://tourist-search.now.sh${endpoint}`, data, {
+  return axios.post(`http://localhost:3100${endpoint}`, data, {
     headers: { "Content-Type": "application/json" }
   });
 };
